d9f771b1ad7e017cccbc34a85a1b8f12bacd8af8
sontsevolosa <sontsevolosa@gmail.com>
2016-03-28 12:48:00 +0300

    Showing all tasks

--- todo.rb
+++ todo.rb
@@ -1,7 +1,33 @@
 class List
+	attr_reader :all_tasks
+
+	def initialize
+		@all_tasks = []
+	end
+
+	def add(task)
+		all_tasks << task
+	end
+
+	def show
+		all_tasks
+	end
+end
+
+class Task
+	attr_reader :description
+
+	def initialize(description)
+		@description = description
+	end
 end
 
 if __FILE__ == $PROGRAM_NAME 
 	my_list = List.new 
-	puts 'You have created a new list' 
+	puts 'You have created a new list'
+	my_list.add(Task.new('Make Breakfast'))
+	my_list.add(Task.new('Make Lunch'))
+	my_list.add(Task.new('Make Dinner'))
+	puts 'You have added a task to the Todo List'
+	puts my_list.show
 end
